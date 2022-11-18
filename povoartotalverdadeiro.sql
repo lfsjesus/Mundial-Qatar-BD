b0a350131a0576f4f8804359cec3a82607f1d445
Miguel Rocha <92723444+r00cha@users.noreply.github.com>
2022-11-18 10:46:35 +0000

    Adicionei o ficheiro para povoar só com dados real

---
+++ povoartotalverdadeiro.sql
@@ -0,0 +1,41 @@
+PRAGMA foreign_keys=ON;
+
+INSERT INTO Equipa (Id, pais) VALUES (1, 'Russia');
+INSERT INTO Equipa (Id, pais) VALUES (2, 'Franca');
+INSERT INTO Equipa (Id, pais) VALUES (3, 'Portugal');
+INSERT INTO Equipa (Id, pais) VALUES (4, 'Alemanha');
+INSERT INTO Equipa (Id, pais) VALUES (5, 'Servia');
+INSERT INTO Equipa (Id, pais) VALUES (6, 'Polonia');
+INSERT INTO Equipa (Id, pais) VALUES (7, 'Inglaterra');
+INSERT INTO Equipa (Id, pais) VALUES (8, 'Espanha');
+INSERT INTO Equipa (Id, pais) VALUES (9, 'Belgica');
+INSERT INTO Equipa (Id, pais) VALUES (10, 'Islandia');
+INSERT INTO Equipa (Id, pais) VALUES (11, 'Suica');
+INSERT INTO Equipa (Id, pais) VALUES (12, 'Croacia');
+INSERT INTO Equipa (Id, pais) VALUES (13, 'Suecia');
+INSERT INTO Equipa (Id, pais) VALUES (14, 'Dinamarca');
+INSERT INTO Equipa (Id, pais) VALUES (15, 'Ira');
+INSERT INTO Equipa (Id, pais) VALUES (16, 'Coreia do Sul');
+INSERT INTO Equipa (Id, pais) VALUES (17, 'Japao');
+INSERT INTO Equipa (Id, pais) VALUES (18, 'Arabia Saudita');
+INSERT INTO Equipa (Id, pais) VALUES (19, 'Australia');
+INSERT INTO Equipa (Id, pais) VALUES (20, 'Tunisia');
+INSERT INTO Equipa (Id, pais) VALUES (21, 'Nigeria');
+INSERT INTO Equipa (Id, pais) VALUES (22, 'Marrocos');
+INSERT INTO Equipa (Id, pais) VALUES (23, 'Senegal');
+INSERT INTO Equipa (Id, pais) VALUES (24, 'Egito');
+INSERT INTO Equipa (Id, pais) VALUES (25, 'Mexico');
+INSERT INTO Equipa (Id, pais) VALUES (26, 'Costa Rica');
+INSERT INTO Equipa (Id, pais) VALUES (27, 'Panama');
+INSERT INTO Equipa (Id, pais) VALUES (28, 'Brasil');
+INSERT INTO Equipa (Id, pais) VALUES (29, 'Uruguai');
+INSERT INTO Equipa (Id, pais) VALUES (30, 'Argentina');
+INSERT INTO Equipa (Id, pais) VALUES (31, 'Colombia');
+INSERT INTO Equipa (Id, pais) VALUES (32, 'Peru');
+
+INSERT INTO FaseDeGrupos (Id, dataInicio, dataFim)
+VALUES (
+    1,
+    '2018-06-14',
+    '2018-06-28'
+  );
