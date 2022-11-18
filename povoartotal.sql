438a987a9d466876bdea44716d82b07bbbbdc94a
Miguel Rocha <92723444+r00cha@users.noreply.github.com>
2022-11-18 15:33:55 +0000

    Fixed errors

--- povoartotal.sql
+++ povoartotal.sql
@@ -71,52 +71,52 @@ INSERT INTO FaseEliminatoria VALUES (4, '2018-07-14' ,'2018-07-15', 'final');
 /* Oitavos de final */
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,/* França */1);
+VALUES (1, 2 /* França */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,30, /* Argentina */2);
+VALUES (1, 30 /* Argentina */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,29, /* Uruguai */1);
+VALUES (1, 29, /* Uruguai */1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,3, /* Portugal */2);
+VALUES (1, 3 /* Portugal */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,28, /* Brasil */1);
+VALUES (1, 28 /* Brasil */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,25, /* Mexico */2);  
+VALUES (1, 25 /* Mexico */, 2);  
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,9, /* Belgica */1);
+VALUES (1, 9 /* Belgica */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,17, /* Japao */2);
+VALUES (1, 17 /* Japao */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,8, /* Espanha */2);
+VALUES (1, 8 /* Espanha */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,1, /* Russia */1);
+VALUES (1, 1 /* Russia */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,12, /* Croacia */1);
+VALUES (1, 12 /* Croacia */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,14, /* Dinamarca */2);
+VALUES (1, 14 /* Dinamarca */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,13, /* Suecia */1);
+VALUES (1, 13 /* Suecia */, 1);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,11, /* Suica */2);
+VALUES (1, 11 /* Suica */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,31, /* Colombia */2);
+VALUES (1, 31 /* Colombia */, 2);
 
 INSERT INTO ClassificacaoEliminatoria (faseElim, equipa, posicao)
-VALUES (1,7, /* Inglaterra */2);
+VALUES (1, 7 /* Inglaterra */, 2);
 
 
 /*Quartos de final*/
