1300b07a70726c8ca65cf4d1df660b6065077959
Luís Jesus <luisjesus__2003@outlook.com>
2022-11-18 02:13:57 +0000

    Update criar.sql
    adicionei constraints (unique) e NOT NULLs. Se o povoar deixar subitamente de funcionar -> voltar à versão anterior.

--- criar.sql
+++ criar.sql
@@ -1,23 +1,23 @@
 DROP TABLE IF EXISTS Estadio;
 CREATE TABLE Estadio (
     Id INT CHECK (Id >= 0),
-    nome TEXT NOT NULL,
+    nome TEXT NOT NULL UNIQUE,
     cidade TEXT NOT NULL,
     PRIMARY KEY (Id)
 );
 DROP TABLE IF EXISTS Evento;
 CREATE TABLE Evento (
     Id INT CHECK (Id >= 0),
-    jogo INT,
+    jogo INT NOT NULL,
     minuto INT CHECK (minuto >= 0),
     PRIMARY KEY (Id),
     FOREIGN KEY (jogo) REFERENCES Jogo(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS Golo;
 CREATE TABLE Golo (
-    evento INT,
-    equipa INT,
-    jogador INT,
+    evento INT NOT NULL,
+    equipa INT NOT NULL,
+    jogador INT NOT NULL,
     tipo TEXT NOT NULL CHECK (tipo IN ('normal', 'penalti', 'auto-golo', 'assistencia')), 
     PRIMARY KEY (evento),
     FOREIGN KEY (evento) REFERENCES Evento(Id) ON UPDATE CASCADE,
@@ -27,30 +27,30 @@ CREATE TABLE Golo (
 );
 DROP TABLE IF EXISTS Substituicao;
 CREATE TABLE Substituicao (
-    evento INT,
+    evento INT NOT NULL,
     PRIMARY KEY (evento),
     FOREIGN KEY (evento) REFERENCES Evento(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS Sai;
 CREATE TABLE Sai (
-    evento INT,
-    jogador INT,
+    evento INT NOT NULL,
+    jogador INT NOT NULL,
     PRIMARY KEY (evento),
     FOREIGN KEY (jogador) REFERENCES Jogador(Id) ON UPDATE CASCADE,
     FOREIGN KEY (evento) REFERENCES Evento(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS Entra;
 CREATE TABLE Entra (
-    evento INT,
-    jogador INT,
+    evento INT NOT NULL,
+    jogador INT NOT NULL,
     PRIMARY KEY (evento),
     FOREIGN KEY (jogador) REFERENCES Jogador(Id) ON UPDATE CASCADE,
     FOREIGN KEY (evento) REFERENCES Evento(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS Cartao;
 CREATE TABLE Cartao (
-    evento INT,
-    jogador INT,
+    evento INT NOT NULL,
+    jogador INT NOT NULL,
     cor VARCHAR(8) NOT NULL,
     PRIMARY KEY (evento),
     FOREIGN KEY (evento) REFERENCES Evento(Id) ON UPDATE CASCADE,
@@ -59,17 +59,17 @@ CREATE TABLE Cartao (
 );
 DROP TABLE IF EXISTS Grupo;
 CREATE TABLE Grupo(
-    Id INT,
-    letra CHAR(1) UNIQUE,
-    faseDeGrupo INT,
+    Id INT NOT NULL,
+    letra CHAR(1) UNIQUE NOT NULL,
+    faseDeGrupo INT NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (faseDeGrupo) REFERENCES FaseDeGrupos(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS Jornada;
 CREATE TABLE Jornada (
-    Id INT,
+    Id INT NOT NULL,
     numero INT NOT NULL CHECK (numero >= 0),
-    grupo INT,
+    grupo INT NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (grupo) REFERENCES Grupo(Id) ON UPDATE CASCADE
 );
@@ -92,7 +92,7 @@ CREATE TABLE FaseEliminatoria (
     Id INT CHECK (Id >= 0),
     dataInicio DATE NOT NULL,
     dataFim DATE NOT NULL,
-    eliminatoria TEXT,
+    eliminatoria TEXT UNIQUE,
     PRIMARY KEY (Id),
     CHECK (dataInicio <= dataFim)
 );
@@ -103,7 +103,7 @@ CREATE TABLE Jogo (
     resultado TEXT NOT NULL,
     jornada INT,
     faseElim INT,
-    estadio INT,
+    estadio INT NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (jornada) REFERENCES Jornada(Id) ON UPDATE CASCADE,
     FOREIGN KEY (faseElim) REFERENCES FaseEliminatoria(Id) ON UPDATE CASCADE,
@@ -112,16 +112,16 @@ CREATE TABLE Jogo (
 );
 DROP TABLE IF EXISTS Jogam;
 CREATE TABLE Jogam(
-    jogo INT,
-    equipa INT,
+    jogo INT NOT NULL,
+    equipa INT NOT NULL,
     PRIMARY KEY (jogo, equipa),
     FOREIGN KEY (jogo) REFERENCES Jogo(Id) ON UPDATE CASCADE,
     FOREIGN KEY (equipa) REFERENCES Equipa(Id) ON UPDATE CASCADE
 );
 DROP TABLE IF EXISTS ClassificacaoEliminatoria;
 CREATE TABLE ClassificacaoEliminatoria (
-    faseElim INT,
-    equipa INT,
+    faseElim INT NOT NULL,
+    equipa INT NOT NULL,
     posicao INT CHECK (posicao <= 16 AND posicao >= 1),
     PRIMARY KEY (faseElim,equipa),
     FOREIGN KEY (faseElim) REFERENCES FaseEliminatoria(Id) ON UPDATE CASCADE,
@@ -129,8 +129,8 @@ CREATE TABLE ClassificacaoEliminatoria (
 );
 DROP TABLE IF EXISTS PontuacaoJornada;
 CREATE TABLE PontuacaoJornada (
-    jornada INT,
-    equipa INT,
+    jornada INT NOT NULL,
+    equipa INT NOT NULL,
     pontos INT CHECK (pontos >= 0),
     PRIMARY KEY (jornada, equipa),
     FOREIGN KEY (jornada) REFERENCES Jornada(Id) ON UPDATE CASCADE,
@@ -144,7 +144,7 @@ CREATE TABLE Jogador (
     nacionalidade TEXT NOT NULL,
     clubeAtual TEXT NOT NULL,
     numero INT CHECK (numero >= 0),
-    equipa INT,
+    equipa INT NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (equipa) REFERENCES Equipa(Id) ON UPDATE CASCADE
 );
@@ -155,7 +155,7 @@ CREATE TABLE Treinador (
     dataNascimento DATE NOT NULL,
     nacionalidade TEXT NOT NULL,
     tipo TEXT NOT NULL,
-    equipa INT,
+    equipa INT NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (equipa) REFERENCES Equipa(Id) ON UPDATE CASCADE,
     CHECK (tipo ='principal' OR tipo ='adjunto')
@@ -170,8 +170,8 @@ CREATE TABLE Arbitro (
 );
 DROP TABLE IF EXISTS TipoArbitro;
 CREATE TABLE TipoArbitro (
-    jogo INT,
-    arbitro INT,
+    jogo INT NOT NULL,
+    arbitro INT NOT NULL,
     tipo TEXT CHECK (tipo ='principal' OR tipo ='adjunto' OR tipo ='video'),
     PRIMARY KEY (jogo, arbitro),
     FOREIGN KEY (jogo) REFERENCES Jogo(Id) ON UPDATE CASCADE,
